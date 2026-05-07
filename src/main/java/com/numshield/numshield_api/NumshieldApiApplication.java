32d6901ee23224bf92b7845f69fd24323958e4a5
Valsuh45 <fuhvalentinesuh@gmail.com>
2026-05-07 12:50:32 +0100

    project initialisation

---
+++ src/main/java/com/numshield/numshield_api/NumshieldApiApplication.java
@@ -0,0 +1,13 @@
+package com.numshield.numshield_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumshieldApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumshieldApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/numshield/numshield_api/NumshieldApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numshield.numshield_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumshieldApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
